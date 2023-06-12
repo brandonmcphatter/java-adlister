2911c5babe3c4157a6d3d95e0319253b37f51345
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-06-12 16:13:25 -0500

    helloworld servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,38 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    private int counter = 0;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        res.setContentType("text/html");
+
+        String name = req.getParameter("name");
+        if (name == null) {
+            out.println("<h1>Hello, World!</h1>");
+        } else {
+            out.println("<h1>Hello, " + name + "!</h1>");
+        }
+
+
+        out.println("<br>");
+
+        String empty = req.getParameter("empty");
+        if (empty == null){
+            counter ++;
+            out.println("<h3>Page Views: " + counter + "</h3>");
+        } else {
+            counter = 1;
+            out.println("<h3>Page Views: " + counter + "</h3>");
+        }
 
     }
 }
